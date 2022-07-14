57096bf3edd7aef7d5597835e7cda905b475f834
Nekai <alessandrobelotti4570@gmail.com>
2022-07-14 16:10:25 +0200

    Fix and update profile pic command.

--- Commands/General/ProfilePic.cs
+++ Commands/General/ProfilePic.cs
@@ -1,19 +1,21 @@
 ﻿namespace CoonBot.Commands {
-    public class ProfilePic : CommandBase {
+	public class ProfilePic : CommandBase {
         [Command("avatar"), Aliases("pfp")]
         [Description("See any user's profile picture.")]
         public async Task AuthorPic(CommandContext msg) {
             if(msg.Member is null) {
-				
                 return;
 			}
             await MemberPic(msg, msg.Member);
         }
 
+        [Command("avatar")]
         public async Task MemberPic(CommandContext msg, [Description("The username or mention of the user to get the profile picture of.")] DiscordMember member) {
             DiscordEmbedBuilder embed = new();
-            embed.ImageUrl = member.AvatarUrl;
-            embed.Title = member.Username;
+			
+            embed.SetDefaults()
+                .WithImageUrl(member.AvatarUrl)
+                .WithTitle(member.Username);
             await msg.Channel.SendMessageAsync(embed.Build());
         }
     }
