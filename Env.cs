e967338267b7cc686a94b2fa85b31af57ec4b1e9
Nekai <alessandrobelotti4570@gmail.com>
2022-07-14 16:08:53 +0200

    Add debug configuration.

--- Env.cs
+++ Env.cs
@@ -5,18 +5,27 @@
 
 		static Env() {
 			Reload();
+#if DEBUG
+			Reload(true);
+#endif
 		}
 
-		public static void Reload() {
+		public static void Reload(bool useDebugFile = false) {
+			string path = useDebugFile
+				? Options.DebugEnvVarsPath
+				: Options.EnvVarsPath;
+			
 			string[] lines;
-			if(!File.Exists(Options.EnvVarsPath)) {
-				File.Create(Options.EnvVarsPath).Close();
+			if(!File.Exists(path)) {
+				File.Create(path).Close();
 				lines = new[] { nameof(YoutubeKey), nameof(DiscordToken) };
-				File.WriteAllLines(Options.EnvVarsPath, lines.Select(x => x + "="));
-				Log.Error($"CRITIAL: Environment file not found. Fill in the {Options.EnvVarsPath} file variables and retry.");
-				Environment.Exit(0);
+				File.WriteAllLines(path, lines.Select(x => x + "="));
+				if(!useDebugFile) {
+					Log.Error($"CRITIAL: Environment file not found. Fill in the {path} file variables and retry.");
+					Environment.Exit(0);
+				}
 			}
-			lines = File.ReadAllLines(Options.EnvVarsPath);
+			lines = File.ReadAllLines(path);
 			
 			// Could handle with reflection
 			foreach(string line in lines) {
@@ -24,7 +33,8 @@
 					.Split('=', 2)
 					.Select(x => x.Trim('\n', ' '))
 					.ToArray();
-				if(parts.Length < 2) 
+				
+				if(parts.Length < 2 || string.IsNullOrWhiteSpace(parts[1])) 
 					continue;
 
 				switch(parts[0]) {
